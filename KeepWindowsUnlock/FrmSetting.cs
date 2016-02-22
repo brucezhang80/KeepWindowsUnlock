54a87b3bdd4423cc34de3bdf44f4a38629d6b7ad
styshoo <styshoo@163.com>
2016-02-22 14:52:17 +0800

    add gitignore, change code namespace

---
+++ KeepWindowsUnlock/FrmSetting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeepWindowsUnlock
+{
+    public partial class FrmSetting : Form
+    {
+        public FrmSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSetting_Load(object sender, EventArgs e)
+        {
+            loadKeys();
+        }
+
+        private void loadKeys()
+        {
+            foreach(dynamic value in Enum.GetValues(typeof(Keys)))
+            {
+                cmbAllKeys.Items.Add(Enum.GetName(typeof(Keys), value));
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+
+    }
+}
